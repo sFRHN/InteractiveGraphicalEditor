b8954354bc1242ab21220a9bb3c149f6e93bb5d3
Farhaan <farhaan654@gmail.com>
2024-11-03 18:40:53 -0600

    Added contains method to box

--- src/main/java/org/example/assignment3/Box.java
+++ src/main/java/org/example/assignment3/Box.java
@@ -19,34 +19,35 @@ public class Box {
         this.y += dy;
     }
 
+    public boolean contains(double mx, double my) {
+        return (this.x <= mx && mx <= this.x + this.width) &&
+                (this.y <= my && my <= this.y + this.height);
+    }
+
+
+    // Getters and Setters
     public double getX() {
         return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public double getHeight() {
+        return height;
     }
 
     public void setX(double x) {
         this.x = x;
     }
-
-    public double getY() {
-        return y;
-    }
-
     public void setY(double y) {
         this.y = y;
     }
-
-    public double getWidth() {
-        return width;
-    }
-
     public void setWidth(double width) {
         this.width = width;
     }
-
-    public double getHeight() {
-        return height;
-    }
-
     public void setHeight(double height) {
         this.height = height;
     }
