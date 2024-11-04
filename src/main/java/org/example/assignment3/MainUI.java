dfa1a8299176dfbcbb32f565b0548144cbcd9ccc
Farhaan <farhaan654@gmail.com>
2024-11-03 23:56:37 -0600

    MainUI class implemented. Connects all other classes

---
+++ src/main/java/org/example/assignment3/MainUI.java
@@ -0,0 +1,23 @@
+package org.example.assignment3;
+
+import javafx.scene.layout.StackPane;
+
+public class MainUI extends StackPane {
+
+    public MainUI() {
+        EntityModel model = new EntityModel();
+        InteractionModel iModel = new InteractionModel();
+        DetailView View = new DetailView();
+        AppController controller = new AppController();
+
+        controller.setModel(model);
+        controller.setiModel(iModel);
+        View.setModel(model);
+        View.setiModel(iModel);
+        View.setupEvents(controller);
+        model.addSubscriber(View);
+
+        this.getChildren().add(View);
+    }
+
+}
