4a32bcfe8913885a3269d0462b7b2b01434c25ba
Farhaan <farhaan654@gmail.com>
2024-11-03 19:07:36 -0600

    Refactoring for visibility

--- src/main/java/org/example/assignment3/Box.java
+++ src/main/java/org/example/assignment3/Box.java
@@ -26,29 +26,13 @@ public class Box {
 
 
     // Getters and Setters
-    public double getX() {
-        return x;
-    }
-    public double getY() {
-        return y;
-    }
-    public double getWidth() {
-        return width;
-    }
-    public double getHeight() {
-        return height;
-    }
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
 
-    public void setX(double x) {
-        this.x = x;
-    }
-    public void setY(double y) {
-        this.y = y;
-    }
-    public void setWidth(double width) {
-        this.width = width;
-    }
-    public void setHeight(double height) {
-        this.height = height;
-    }
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+    public void setWidth(double width) { this.width = width; }
+    public void setHeight(double height) { this.height = height; }
 }
