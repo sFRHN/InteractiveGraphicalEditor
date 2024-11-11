a541812c6ed4b3a732a4d19583d4a8281a61505e
Farhaan <farhaan654@gmail.com>
2024-11-11 00:17:23 -0600

    Set MiniView to use MiniController instead of normal controller

--- src/main/java/org/example/assignment3/MainUI.java
+++ src/main/java/org/example/assignment3/MainUI.java
@@ -11,17 +11,25 @@ public class MainUI extends StackPane {
         DetailView View = new DetailView();
         MiniView miniView = new MiniView();
         AppController controller = new AppController();
+        MiniController miniController = new MiniController();
 
         controller.setModel(model);
         controller.setiModel(iModel);
+
+        miniController.setModel(model);
+        miniController.setiModel(iModel);
+
         View.setModel(model);
         View.setiModel(iModel);
         View.setupEvents(controller);
+
         miniView.setModel(model);
         miniView.setiModel(iModel);
-        miniView.setupEvents(controller);
+        miniView.setupEvents(miniController);
+
         model.addSubscriber(View);
         model.addSubscriber(miniView);
+
         iModel.addSubscriber(View);
         iModel.addSubscriber(miniView);
 
