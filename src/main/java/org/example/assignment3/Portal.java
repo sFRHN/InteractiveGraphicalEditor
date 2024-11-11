debf0b3bbda9adf3d20b24ce3376fcd93098af44
Farhaan <farhaan654@gmail.com>
2024-11-11 02:15:23 -0600

    Portal class updated - Override changePosition to change pLeft and pTop as well - Override move to change pLeft and pTop as well - Added more basic getters and setters

--- src/main/java/org/example/assignment3/Portal.java
+++ src/main/java/org/example/assignment3/Portal.java
@@ -1,22 +1,32 @@
 package org.example.assignment3;
 
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-
 public class Portal extends Box {
 
     private double scale;
+    private double pLeft, pTop;
 
     public Portal(double x, double y, double width, double height) {
         super(x, y, width, height);
         scale = 0.5;
     }
 
+    public void changePosition(double x, double y) {
+        super.changePosition(x, y);
+        pLeft = x;
+        pTop = y;
+    }
 
+    public void move(double dX, double dY) {
+        super.move(dX, dY);
+        pLeft += dX;
+        pTop += dY;
+    }
 
     public double getScale() { return scale; }
-
+    public double getPLeft() { return pLeft; }
+    public double getPTop() { return pTop; }
 
     public void setScale(double scale) { this.scale = scale; }
-
+    public void setPLeft(double pLeft) { this.pLeft = pLeft; }
+    public void setPTop(double pTop) { this.pTop = pTop; }
 }
